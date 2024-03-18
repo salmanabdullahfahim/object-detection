b89d2496bc0883911e60cccb31b8a0ee5fea1a30
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-03-18 15:15:19 +0600

    Add webcam video size adjustment and show video function.

--- components/ObjectDetection.jsx
+++ components/ObjectDetection.jsx
@@ -1,14 +1,33 @@
 "use client";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import Webcam from "react-webcam";
 
 const ObjectDetection = () => {
+  const webCamRef = useRef(null);
+
+  const showMyVideo = () => {
+    if (
+      webCamRef.current !== null &&
+      webCamRef.current.video?.readyState === 4
+    ) {
+      const myVideWidth = webCamRef.current.video.videoWidth;
+      const myVideHeight = webCamRef.current.video.videoHeight;
+
+      webCamRef.current.video.width = myVideWidth;
+      webCamRef.current.video.height = myVideHeight;
+    }
+  };
+
+  useEffect(() => {
+    showMyVideo();
+  }, []);
   return (
     <div className="mt-8">
       <div className="relative flex justify-center items-center gradiant p-1.5 rounded-md">
         {/* webcam */}
         <Webcam
-          className="rounded-md w-full lg:h-[720px]"
+          ref={webCamRef}
+          className="rounded-md w-full lg:h-[650px]"
           muted
           mirrored={true}
         />
