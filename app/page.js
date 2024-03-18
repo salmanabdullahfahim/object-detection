4075a91879006dfcce6a6d12adc04df3eb80d7cd
salmanabdullahfahim <salmanabdullah0304@gmail.com>
2024-03-18 14:50:08 +0600

    Update CSS and layout components

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex flex-col items-center min-h-screen p-8">
-      <h2 className="font-extrabold text-3xl md:text-4xl lg:text-6xl md:px-6 tracking-tighter text-center">
+      <h2 className="gradiant-title font-extrabold text-3xl md:text-4xl lg:text-6xl md:px-6 tracking-tighter text-center">
         AI Object Detection Alarm
       </h2>
     </main>
